24eeb15a9ed4c0866c57bc9b23a236b784640471
MhZ9191 <hegic91@gmail.com>
2026-02-12 16:34:08 +0100

    Added select filter

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,3 +1,5 @@
+import { useState, useEffect } from "react";
+
 export default function Main({ films }) {
   //array con i generi dei film senza duplicati
   const generi = films.reduce((acc, ce) => {
@@ -8,6 +10,33 @@ export default function Main({ films }) {
     return acc;
   }, []);
 
+  //useState
+  const [indexSearch, setIndexSearch] = useState(null);
+  const [filterList, setFilterList] = useState(films);
+  const [mostraTutti, setMostraTutti] = useState(true);
+
+  //Logic
+  const clicked = (indice) => {
+    setMostraTutti(false);
+    setIndexSearch(indice); //al click prendo l'indice che corrisponde al genere del mio array "generi", per poi poterlo usare nel filter
+  };
+
+  const allList = () => {
+    setMostraTutti(true); //al click lo reimposto a true cosi da mostrare tutti i film
+  };
+
+  //useEffect
+  useEffect(() => {
+    if (mostraTutti) {
+      //se mostraTutti è true non eseguo nessun filter
+      setFilterList(films);
+    } else {
+      //altrimenti filtro in base al genere
+      const tmpList = films.filter((el) => el.genre === generi[indexSearch]); //paragono i generi, non ho eseguito "tolowercase()" perchè l'array di stringhe in generi è preso da genre di films
+      setFilterList(tmpList);
+    }
+  }, [indexSearch, mostraTutti]); //variabili su cui osservarne i cambiamenti
+
   return (
     <>
       <main>
@@ -16,19 +45,26 @@ export default function Main({ films }) {
             <div>
               <label htmlFor="genere">GENERE</label>
               <select name="genere" id="genere">
-                <option value="" disabled selected>
-                  Seleziona una categoria
+                <option value="tutti" onClick={allList}>
+                  MOSTRA TUTTI
                 </option>
-                <option value="tutti">MOSTRA TUTTI</option>
-                {generi.map((genere) => {
-                  return <option value="genere">{genere.toUpperCase()}</option>;
+                {generi.map((genere, ind) => {
+                  return (
+                    <option
+                      key={ind}
+                      value="genere"
+                      onClick={() => clicked(ind)}
+                    >
+                      {genere.toUpperCase()}
+                    </option>
+                  );
                 })}
               </select>
             </div>
             <div>
               <ul>
-                {films.map(({ title }) => {
-                  return <li>{title}</li>;
+                {filterList.map(({ title }, index) => {
+                  return <li key={index}>{title}</li>;
                 })}
               </ul>
             </div>
